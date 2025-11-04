35eb4df461bb6083735ceb5438847d4340c4a838
Hamed <ostovari.eng@gmail.com>
2025-11-04 22:55:00 +0100

    Updated tailwind variables, WIP: implemented Navbar component, implemented nonDashboard layout

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import { usePathname, useRouter } from "next/navigation";
+import { SidebarTrigger } from "./ui/sidebar";
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const isDashboardPage =
+    pathname.includes("/managers") || pathname.includes("/tenants");
+  return (
+    <div
+      className="fixed top-0 left-0 w-full z-50 shadow-xl"
+      style={{ height: `${NAVBAR_HEIGHT}px` }}
+    >
+      <div className="flex justify-between items-center w-full py-3 px-8 bg-primary  text-white">
+        <div className="flex items-center gap-4 md:gap-6">
+          {isDashboardPage && (
+            <div className="md:hidden">
+              <SidebarTrigger />
+            </div>
+          )}
+          <Link
+            href="/"
+            className="cursor-pointer hover:!text-primary-300"
+            scroll={false}
+          >
+            <div className="flex items-center gap-3">
+              <Image
+                src="/images/logo.svg"
+                alt="Rentiful Logo"
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="text-xl font-bold">
+                RENT
+                <span className="text-secondary-500 font-light hover:!text-primary-300">
+                  IFUL
+                </span>
+              </div>
+            </div>
+          </Link>
+        </div>
+        {!isDashboardPage && (
+          <p className="text-primary-200 hidden md:block">
+            Discover your perfect rental apartment with our advanced search
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
