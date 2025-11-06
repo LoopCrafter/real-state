9aae512a8c7e90f46eb105d97d5742c4de2d4ea5
Hamed <ostovari.eng@gmail.com>
2025-11-06 20:35:40 +0100

    Implemeted Server

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => {
+  console.log(`listen on port ${PORT}`);
+});
